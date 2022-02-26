23c2e158ffa8b1e0365627ffab3ab8b89dfa53f2
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-26 00:20:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+wallet = 40
+
+wallet -= 8 #I bought some food
+
+wallet += 40 # I got a payday!
+
+#I need to figure out how to uppercase this
+'nick'.upper()
+
+# This course has been very fun and informative! 
